bae0e8231124c4220db4d71b204263e7613791b8
James Cerone <jimmy.cerone@gmail.com>
2025-01-20 10:11:02 -0600

    fix format for questions

--- csvToForm.py
+++ csvToForm.py
@@ -2,12 +2,13 @@ from apiclient import discovery
 from httplib2 import Http
 from oauth2client import client, file, tools
 import argparse
+from parseCsv import parse_csv
+from formatForGoogleFormApi import formatForGoogleFormApi
 
 SCOPES = "https://www.googleapis.com/auth/forms.body"
 DISCOVERY_DOC = "https://forms.googleapis.com/$discovery/rest?version=v1"
     
 title = input("Enter the title: ")
-description = input("Enter the description: ")
 
 store = file.Storage("token.json")
 creds = None
@@ -27,7 +28,6 @@ form_service = discovery.build(
 NEW_FORM = {
     "info": {
         "title": title,
-        "description": description
     }
 }
 
@@ -39,10 +39,12 @@ csv = parse_csv('test.csv')
 
 questions = formatForGoogleFormApi(csv)
 
+body = { "requests": questions }
+
 # Adds the question to the form
 question_setting = (
     form_service.forms()
-    .batchUpdate(formId=result["formId"], body=questions)
+    .batchUpdate(formId=result["formId"], body=body)
     .execute()
 )
 
